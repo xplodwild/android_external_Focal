b21d265ebbed819800c8b2706348901dae215526
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-03 08:45:50 +0530

    exiv2: cleanup

--- exiv2/Android.mk
+++ exiv2/Android.mk
@@ -7,56 +7,59 @@
 LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
 
-CCSRC =  src/basicio.cpp           \
-	 src/bmpimage.cpp          \
-	 src/canonmn.cpp           \
-	 src/convert.cpp           \
-	 src/cr2image.cpp          \
-	 src/crwimage.cpp          \
-	 src/datasets.cpp          \
-	 src/easyaccess.cpp        \
-	 src/epsimage.cpp          \
-	 src/error.cpp             \
-	 src/exif.cpp              \
-	 src/futils.cpp            \
-	 src/fujimn.cpp            \
-	 src/gifimage.cpp          \
-	 src/image.cpp             \
-	 src/iptc.cpp              \
-	 src/jp2image.cpp          \
-	 src/jpgimage.cpp          \
-	 src/makernote.cpp         \
-	 src/metadatum.cpp         \
-	 src/minoltamn.cpp         \
-	 src/mrwimage.cpp          \
-	 src/nikonmn.cpp           \
-	 src/olympusmn.cpp         \
-	 src/orfimage.cpp          \
-	 src/panasonicmn.cpp       \
-	 src/pgfimage.cpp
+CCSRC := \
+	src/basicio.cpp \
+	src/bmpimage.cpp \
+	src/canonmn.cpp \
+	src/convert.cpp \
+	src/cr2image.cpp \
+	src/crwimage.cpp \
+	src/datasets.cpp \
+	src/easyaccess.cpp \
+	src/epsimage.cpp \
+	src/error.cpp \
+	src/exif.cpp \
+	src/futils.cpp \
+	src/fujimn.cpp \
+	src/gifimage.cpp \
+	src/image.cpp \
+	src/iptc.cpp \
+	src/jp2image.cpp \
+	src/jpgimage.cpp \
+	src/makernote.cpp \
+	src/metadatum.cpp \
+	src/minoltamn.cpp \
+	src/mrwimage.cpp \
+	src/nikonmn.cpp \
+	src/olympusmn.cpp \
+	src/orfimage.cpp \
+	src/panasonicmn.cpp \
+	src/pgfimage.cpp
 
-CCSRC += src/pngimage.cpp          \
-	 src/pngchunk.cpp
+CCSRC += \
+	src/pngimage.cpp \
+	src/pngchunk.cpp
 
-CCSRC += src/preview.cpp           \
-	 src/properties.cpp        \
-	 src/psdimage.cpp          \
-	 src/rafimage.cpp          \
-	 src/rw2image.cpp          \
-	 src/samsungmn.cpp         \
-	 src/sigmamn.cpp           \
-	 src/pentaxmn.cpp          \
-	 src/sonymn.cpp            \
-	 src/tags.cpp              \
-	 src/tgaimage.cpp          \
-	 src/tiffcomposite.cpp     \
-	 src/tiffimage.cpp         \
-	 src/tiffvisitor.cpp       \
-	 src/types.cpp             \
-	 src/value.cpp             \
-	 src/version.cpp           \
-	 src/xmp.cpp               \
-	 src/xmpsidecar.cpp
+CCSRC += \
+	src/preview.cpp \
+	src/properties.cpp \
+	src/psdimage.cpp \
+	src/rafimage.cpp \
+	src/rw2image.cpp \
+	src/samsungmn.cpp \
+	src/sigmamn.cpp \
+	src/pentaxmn.cpp \
+	src/sonymn.cpp \
+	src/tags.cpp \
+	src/tgaimage.cpp \
+	src/tiffcomposite.cpp \
+	src/tiffimage.cpp \
+	src/tiffvisitor.cpp \
+	src/types.cpp \
+	src/value.cpp \
+	src/version.cpp \
+	src/xmp.cpp \
+	src/xmpsidecar.cpp
 
 LOCAL_SRC_FILES:= $(CCSRC)
 
@@ -67,10 +70,10 @@ LOCAL_SHARED_LIBRARIES :=
 LOCAL_STATIC_LIBRARIES := 
 LOCAL_LDLIBS := -lz -lc
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+LOCAL_C_INCLUDES := \
+	$(LOCAL_PATH) \
 	$(LOCAL_PATH)/../libtiff \
 	external/zlib
-
 
 LOCAL_CFLAGS := -DAVOID_TABLES -O3 \
 	-DANDROID_TILE_BASED_DECODE -DENABLE_ANDROID_NULL_CONVERT -D__Ansi__ \
