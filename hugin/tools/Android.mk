4452643799288bc9610fca37a9feb7940bdcd813
Guillaume Lesniak <xplodwild@cyanogenmod.org>
2013-07-03 03:56:26 +0200

    Build pto_gen

--- hugin/tools/Android.mk
+++ hugin/tools/Android.mk
@@ -64,3 +64,25 @@ LOCAL_MODULE := pano_modify
 
 include $(BUILD_EXECUTABLE)
 
+######################################################
+###                    pto_gen                      ##
+######################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := pto_gen.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+	$(LOCAL_PATH)/../../vigra/include \
+	$(LOCAL_PATH)/../../boost-1_53/ \
+	$(LOCAL_PATH)/../../exiv2/include \
+	$(LOCAL_PATH)/../../libpano13 \
+	$(LOCAL_PATH)/../../lensfun/include/lensfun \
+	$(LOCAL_PATH)/..
+
+LOCAL_SHARED_LIBRARIES := libhugin libvigraimpex libpano13
+
+LOCAL_MODULE := pto_gen
+
+include $(BUILD_EXECUTABLE)
+
