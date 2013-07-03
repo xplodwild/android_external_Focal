f8e349944957059f37e114f5d0facabc680b8988
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-03 12:26:57 +0530

    get rid of libsift

--- apsc/Android.mk
+++ apsc/Android.mk
@@ -1,69 +1,22 @@
 # Makefile for autopano tools
 
 ######################################################
-###                    libsift                      ##
+###                   autopano                      ##
 ######################################################
 
 LOCAL_PATH := $(my-dir)
 include $(CLEAR_VARS)
 
-libsift_SOURCES := \
+LOCAL_SRC_FILES := \
 	LoweDetector.c RANSAC.c GaussianConvolution.c \
 	ScaleSpace.c KeypointXML.c MatchKeys.c KDTree.c BondBall.c \
 	AreaFilter.c ImageMatchModel.c Transform.c DisplayImage.c ImageMap.c \
-	HashTable.c ArrayList.c Random.c SimpleMatrix.c Utils.c
+	HashTable.c ArrayList.c Random.c SimpleMatrix.c Utils.c \
+	AutoPano.c
 
 LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-
-LOCAL_SRC_FILES:= $(libsift_SOURCES)
-
-LOCAL_C_INCLUDES := $(LOCAL_PATH) \
-	$(LOCAL_PATH)/include \
-	$(LOCAL_PATH)/../libtiff \
-	$(LOCAL_PATH)/../libpano13
-
-ifeq ($(ANDROID_BUILD_TOP),)
-LOCAL_C_INCLUDES += \
-	$(LOCAL_PATH)/../libxml2/include \
-	$(LOCAL_PATH)/../libiconv/include
-else
-LOCAL_C_INCLUDES += \
-	external/libxml2/include \
-	external/icu4c/common
-endif
-
-LOCAL_CFLAGS := -DAVOID_TABLES -O3 -fexceptions -fstrict-aliasing -fprefetch-loop-arrays \
-	-D__Ansi__ -D_GNU_SOURCE -DHAS_PANO13
-
-LOCAL_STATIC_LIBRARIES := libxml2
-ifeq ($(ANDROID_BUILD_TOP),)
-LOCAL_STATIC_LIBRARIES += libiconv
-else
-LOCAL_SHARED_LIBRARIES += libicuuc
-endif
-
-LOCAL_LDLIBS := -lxml
-LOCAL_MODULE_TAGS := debug
-
-LOCAL_MODULE := libsift
-
-include $(BUILD_STATIC_LIBRARY)
-
-######################################################
-###                   autopano                      ##
-######################################################
- 
-include $(CLEAR_VARS)
-
-autopano_SOURCES := AutoPano.c
-
-LOCAL_SDK_VERSION := 14
-
-LOCAL_SRC_FILES:= $(autopano_SOURCES) $(libsift_SOURCES)
 
 LOCAL_SHARED_LIBRARIES := libtiffdecoder libpano13
-LOCAL_STATIC_LIBRARIES := libsift
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	$(LOCAL_PATH)/include \
