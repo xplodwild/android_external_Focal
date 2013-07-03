2d8aca04503da82409333f834bfedd0c5323e8db
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-03 08:30:45 +0530

    glib: cleanup

--- glib/Android.mk
+++ glib/Android.mk
@@ -6,37 +6,37 @@ GLIB_TOP := $(LOCAL_PATH)
 
 GLIB_BUILD_STATIC := $(BUILD_STATIC)
 
-GLIB_C_INCLUDES :=			\
-	$(GLIB_TOP)			\
-	$(GLIB_TOP)/glib		\
+GLIB_C_INCLUDES := \
+	$(GLIB_TOP) \
+	$(GLIB_TOP)/glib \
 	$(GLIB_TOP)/android
 
-GTHREAD_C_INCLUDES :=			\
-	$(GLIB_C_INCLUDES)		\
+GTHREAD_C_INCLUDES := \
+	$(GLIB_C_INCLUDES) \
 	$(GLIB_TOP)/gthread
 
-GOBJECT_C_INCLUDES :=			\
-	$(GLIB_C_INCLUDES)		\
-	$(GLIB_TOP)/gobject		\
+GOBJECT_C_INCLUDES := \
+	$(GLIB_C_INCLUDES) \
+	$(GLIB_TOP)/gobject \
 	$(GLIB_TOP)/gobject/android
 
-GMODULE_C_INCLUDES :=			\
-	$(GLIB_C_INCLUDES)		\
+GMODULE_C_INCLUDES := \
+	$(GLIB_C_INCLUDES) \
 	$(GLIB_TOP)/gmodule
 
-GIO_C_INCLUDES :=			\
-	$(GLIB_C_INCLUDES)		\
-	$(GLIB_TOP)/gio			\
+GIO_C_INCLUDES := \
+	$(GLIB_C_INCLUDES) \
+	$(GLIB_TOP)/gio \
 	$(GLIB_TOP)/gio/android
 
-GLIB_SHARED_LIBRARIES :=		\
-	libgmodule-2.0			\
-	libgobject-2.0			\
-	libgthread-2.0			\
+GLIB_SHARED_LIBRARIES := \
+	libgmodule-2.0 \
+	libgobject-2.0 \
+	libgthread-2.0 \
 	libglib-2.0
 
-GLIB_STATIC_LIBRARIES :=		\
-	$(GLIB_SHARED_LIBRARIES)	\
+GLIB_STATIC_LIBRARIES := \
+	$(GLIB_SHARED_LIBRARIES) \
 	libpcre
 
 include $(CLEAR_VARS)
