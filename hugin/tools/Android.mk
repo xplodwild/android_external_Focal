ef768499b352407ec93af5c8deb39e23b8b37814
xplodwild <xplodgui@gmail.com>
2013-07-08 20:27:02 +0200

    Build align_image_stack

--- hugin/tools/Android.mk
+++ hugin/tools/Android.mk
@@ -16,18 +16,9 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	$(LOCAL_PATH)/../../libpano13 \
 	$(LOCAL_PATH)/..
 
-LOCAL_SHARED_LIBRARIES := libpano13 libgmodule-2.0 \
-	libgobject-2.0 libgthread-2.0 libglib-2.0 libvigraimpex \
-	libexiv2
-LOCAL_STATIC_LIBRARIES := libhugin liblensfun libboost_filesystem-gcc-mt-1_53 \
-	libboost_system-gcc-mt-1_53
+LOCAL_STATIC_LIBRARIES := libhugin
 
 LOCAL_MODULE := ptclean
-
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-
-LOCAL_CFLAGS := -O3 -fexceptions -frtti
 
 include $(BUILD_EXECUTABLE)
 
@@ -47,18 +38,10 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	$(LOCAL_PATH)/../../libtiff \
 	$(LOCAL_PATH)/..
 
-LOCAL_SHARED_LIBRARIES := libpano13 libgmodule-2.0 \
-	libgobject-2.0 libgthread-2.0 libglib-2.0 libvigraimpex \
-	libexiv2
-LOCAL_STATIC_LIBRARIES := libhugin liblensfun libboost_filesystem-gcc-mt-1_53 \
-	libboost_system-gcc-mt-1_53
+LOCAL_SHARED_LIBRARIES := libvigraimpex libpano13
+LOCAL_STATIC_LIBRARIES := libhugin
 
 LOCAL_MODULE := autooptimiser
-
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-
-LOCAL_CFLAGS := -O3 -fexceptions -frtti -Wno-sign-compare -Wno-sign-promo -Wno-non-virtual-dtor
 
 include $(BUILD_EXECUTABLE)
 
@@ -77,18 +60,10 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	$(LOCAL_PATH)/../../libpano13 \
 	$(LOCAL_PATH)/..
 
-LOCAL_SHARED_LIBRARIES := libpano13 libgmodule-2.0 \
-	libgobject-2.0 libgthread-2.0 libglib-2.0 libvigraimpex \
-	libexiv2
-LOCAL_STATIC_LIBRARIES := libhugin liblensfun libboost_filesystem-gcc-mt-1_53 \
-	libboost_system-gcc-mt-1_53
+LOCAL_SHARED_LIBRARIES := libvigraimpex libpano13
+LOCAL_STATIC_LIBRARIES := libhugin
 
 LOCAL_MODULE := pano_modify
-
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-
-LOCAL_CFLAGS := -O3 -fexceptions -frtti
 
 include $(BUILD_EXECUTABLE)
 
@@ -109,18 +84,10 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	$(LOCAL_PATH)/../../lensfun/include/lensfun \
 	$(LOCAL_PATH)/..
 
-LOCAL_SHARED_LIBRARIES := libpano13 libgmodule-2.0 \
-	libgobject-2.0 libgthread-2.0 libglib-2.0 libvigraimpex \
-	libexiv2
-LOCAL_STATIC_LIBRARIES := libhugin liblensfun libboost_filesystem-gcc-mt-1_53 \
-	libboost_system-gcc-mt-1_53
+LOCAL_SHARED_LIBRARIES := libvigraimpex libpano13
+LOCAL_STATIC_LIBRARIES := libhugin
 
 LOCAL_MODULE := pto_gen
-
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-
-LOCAL_CFLAGS := -O3 -fexceptions -frtti
 
 #include $(BUILD_EXECUTABLE)
 
@@ -146,18 +113,38 @@ LOCAL_C_INCLUDES := $(LOCAL_PATH) \
 	$(LOCAL_PATH)/../../lensfun/include/lensfun \
 	$(LOCAL_PATH)/../
 
-LOCAL_SHARED_LIBRARIES := libpano13 libgmodule-2.0 \
-	libgobject-2.0 libgthread-2.0 libglib-2.0 libvigraimpex \
-	libexiv2
-LOCAL_STATIC_LIBRARIES := libhugin liblensfun libboost_filesystem-gcc-mt-1_53 \
-	libboost_system-gcc-mt-1_53 libboost_thread-gcc-mt-1_53
+LOCAL_SHARED_LIBRARIES := libvigraimpex libpano13
+LOCAL_STATIC_LIBRARIES := libhugin libboost_thread-gcc-mt-1_53
 
 LOCAL_MODULE := nona
 
-LOCAL_SDK_VERSION := 14
-LOCAL_NDK_STL_VARIANT := gnustl_static
-
-LOCAL_CFLAGS := -O3 -fexceptions -frtti
-
 include $(BUILD_EXECUTABLE)
 
+######################################################
+###               align_image_stack                 ##
+######################################################
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := align_image_stack.cpp \
+	../nona/Stitcher.cpp \
+	../nona/Stitcher1.cpp \
+	../nona/Stitcher2.cpp \
+	../nona/Stitcher3.cpp \
+	../nona/Stitcher4.cpp
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+	$(LOCAL_PATH)/../../vigra/include \
+	$(LOCAL_PATH)/../../boost-1_53/ \
+	$(LOCAL_PATH)/../../libtiff \
+	$(LOCAL_PATH)/../../exiv2/include \
+	$(LOCAL_PATH)/../../libpano13 \
+	$(LOCAL_PATH)/../../lensfun/include/lensfun \
+	$(LOCAL_PATH)/../
+
+LOCAL_SHARED_LIBRARIES := libvigraimpex libpano13
+LOCAL_STATIC_LIBRARIES := libhugin libboost_thread-gcc-mt-1_53
+
+LOCAL_MODULE := align_image_stack
+
+include $(BUILD_EXECUTABLE)
