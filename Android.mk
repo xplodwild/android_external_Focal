21ba5c213338481f7c11d3f45eaed0382db2fb80
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-15 02:19:39 +0530

    add gnustl
    * Uses prebuilt binaries, and headers from prebuilts/ndk

--- Android.mk
+++ Android.mk
@@ -21,3 +21,36 @@ ifeq ($(ANDROID_BUILD_TOP),)
 	include $(NEMESIS_JNI_PATH)/lensfun/Android.mk
 	include $(NEMESIS_JNI_PATH)/celeste/Android.mk
 endif
+
+ifneq ($(ANDROID_BUILD_TOP),)
+
+#TODO: Support architectures other than ARM
+GNUSTL_PATH := ../../prebuilts/ndk/current/sources/cxx-stl/gnu-libstdc++/libs/armeabi-v7a/
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgnustl_static
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_SRC_FILES := $(GNUSTL_PATH)/libgnustl_static.a
+include $(LOCAL_PATH)/gnustl.mk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libsupc++
+LOCAL_MODULE_CLASS := STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX := .a
+LOCAL_SRC_FILES := $(GNUSTL_PATH)/libsupc++.a
+include $(LOCAL_PATH)/gnustl.mk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libgnustl_shared
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_SRC_FILES := $(GNUSTL_PATH)/libgnustl_shared.so
+include $(LOCAL_PATH)/gnustl.mk
+include $(BUILD_PREBUILT)
+
+endif
---
+++ gnustl.mk
@@ -0,0 +1,16 @@
+# Add a couple include paths to use gnustl.
+
+# Make sure bionic is first so we can include system headers.
+LOCAL_C_INCLUDES := \
+	bionic \
+	prebuilts/ndk/current/sources/cxx-stl/gnu-libstdc++/include \
+	prebuilts/ndk/current/sources/cxx-stl/gnu-libstdc++/libs/armeabi-v7a/include \
+	$(LOCAL_C_INCLUDES)
+
+ifeq ($(LOCAL_NDK_STL_VARIANT),gnustl_static)
+LOCAL_STATIC_LIBRARIES := libgnustl_static libsupc++ $(LOCAL_STATIC_LIBRARIES)
+endif
+ifeq ($(LOCAL_NDK_STL_VARIANT),gnustl_shared)
+LOCAL_STATIC_LIBRARIES := libsupc++ $(LOCAL_STATIC_LIBRARIES)
+LOCAL_SHARED_LIBRARIES := libgnustl_shared $(LOCAL_SHARED_LIBRARIES)
+endif
