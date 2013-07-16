aee1f91ee35de52a6ea793d65559b18a1a52c8df
Chirayu Desai <cdesai@cyanogenmod.org>
2013-07-16 19:05:56 +0530

    glib: nothing to see here, move along
    * Sshhh, don't look in here, go away.

--- glib/glib/gbitlock.c
+++ glib/glib/gbitlock.c
@@ -55,6 +55,8 @@ _g_futex_thread_init (void) {
  *
  * If anyone actually gets bit by this, please file a bug. :)
  */
+#define __user 
+#include <sys/linux-syscalls.h>
 #include <linux/futex.h>
 #include <sys/syscall.h>
 #include <unistd.h>
@@ -80,7 +82,7 @@ static void
 g_futex_wait (const volatile gint *address,
               gint                 value)
 {
-  syscall (SYS_futex, address, (gsize) FUTEX_WAIT, (gsize) value, NULL);
+  syscall (__NR_futex, address, (gsize) FUTEX_WAIT, (gsize) value, NULL);
 }
 
 /* < private >
@@ -97,7 +99,7 @@ g_futex_wait (const volatile gint *address,
 static void
 g_futex_wake (const volatile gint *address)
 {
-  syscall (SYS_futex, address, (gsize) FUTEX_WAKE, (gsize) 1, NULL);
+  syscall (__NR_futex, address, (gsize) FUTEX_WAKE, (gsize) 1, NULL);
 }
 
 #else
